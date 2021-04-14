3bd7cd94ec5b3885c7cea8effbb1e98bb9db3e90
b-gran <bill@billgranfield.com>
2021-04-13 22:17:17 -0400

    random util for uninstalling vital

---
+++ uninstall_vital.sh
@@ -0,0 +1,9 @@
+sudo rm -rf /Applications/Vital.app
+sudo rm -rf ~/Library/Application\ Support/vital
+sudo rm -rf ~/Library/Caches/Vital
+sudo rm -rf ~/Library/Cookies/audio.vital.standalone.binarycookies  
+sudo rm -rf ~/Library/Preferences/audio.vital.standalone.plist
+sudo rm -rf ~/Library/Saved Application State/audio.vital.standalone.savedState
+sudo rm -rf /Library/Audio/Plug-Ins/Components/Vital.component
+sudo rm -rf /Library/Audio/Plug-Ins/VST/Vital.vst
+sudo rm -rf /Library/Audio/Plug-Ins/VST3/Vital.vst3
